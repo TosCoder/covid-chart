1232bc58b492bb54fdbbfd9c101af3417afe40f1
TosCoder <loohincoder@gmail.com>
2020-08-27 12:30:38 +0700

    init commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react'
+
+class Home extends Component{
+    render(){
+        return(
+            <Fragment>HOME</Fragment>
+        )
+    }
+}
+
+export  default Home
